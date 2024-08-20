9e07aff1b00d5d1c2d6c37adbeba9514ffc6c2da
ankit-chauhan-codes <101314144+ankit-chauhan-codes@users.noreply.github.com>
2024-08-20 15:43:55 +0530

    added font-family and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+
+    screens: {
+      'xs': '375px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+
+    fontFamily: {
+      altivo: ['altivo', 'sans-serif'],
+      bai: ['bai-jamjuree', 'sans-serif'],
+    },
+
+    extend: {
+
+      colors: {
+        'primary': '#7B9645',
+        'secondary': '#53514D',
+        'light': '#fff',
+        'dark': '#201818',
+      },
+
+    },
   },
   plugins: [],
 }
